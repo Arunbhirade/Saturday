163372369e1bc561127fe2ab1ec1c5ee6597f86a
Arunbhirade <arunbhirade0707@gmail.com>
2023-07-29 17:28:25 +0530

    Circle program Pushing

---
+++ Circle.java
@@ -0,0 +1,15 @@
+class Circle 
+{
+	static void area()
+	{
+	int r=10;
+	final double pi=3.14;
+	double area=pi*r*r;
+	System.out.println("The area of Circle is "+area);
+	}
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World!");
+		area();
+	}
+}
