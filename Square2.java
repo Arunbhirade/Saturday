bc9e14d9fe8fd3cb1e6c55d1aeed79b6591a9fa6
Arunbhirade <arunbhirade0707@gmail.com>
2023-07-29 17:37:37 +0530

    Square code Pushing

---
+++ Square2.java
@@ -0,0 +1,14 @@
+class  Square2
+{
+	static int area()
+	{
+	int a=10;
+	int result=a+a;
+	return result;
+	}
+	public static void main(String[] args) 
+	{
+		int x=area();
+		System.out.println("The area of square is "+x);
+	}
+}
